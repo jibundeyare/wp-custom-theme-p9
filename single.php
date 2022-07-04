fc215c149909d57585838ed7cebcbb87ed17b4f0
popschool <contact@jibundeyare.com>
2022-07-04 16:25:34 +0200

    Améliorations

--- single.php
+++ single.php
@@ -10,6 +10,11 @@ if (have_posts()):
     ?>
         <article class="bg-slate-400">
             <h2><?php the_title() ?></h2>
+            <?php if ( has_post_thumbnail() ): ?>
+            <div>
+                <?php the_post_thumbnail( 'large' ); ?>
+            </div>
+            <?php endif ?>
             <p><?php the_content() ?></p>
         </article>
     <?php
